2b012ec5235605cd8340960b056211acb33df4a3
Luis Ortiz <ortizfrancoluisjavier@gmailcom>
2024-06-25 10:41:47 -0500

    se corrige bug al extraer organizationId de los headers

--- src/common/decorators/active-user.decorator.ts
+++ src/common/decorators/active-user.decorator.ts
@@ -5,5 +5,7 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 export const ActiveUser = createParamDecorator(
     (data: unknown, ctx: ExecutionContext)=>{
         const request = ctx.switchToHttp().getRequest();
-        return request.user;
+        const newUser = request.user
+        newUser.organizationId = request.headers.organizationid
+        return newUser;
 });
