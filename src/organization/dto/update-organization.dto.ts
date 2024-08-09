a46d59d7539c4e8d8ff591999e3d7f43de9a3430
carlos rubio <carlos.rubio@mocion.com.co>
2024-08-09 15:26:08 -0500

    refactor: Update organization DTO with contact email, phone, website, and social media
    The `UpdateOrganizationDto` has been modified to include fields for contact email, phone, website URL, and social media. This refactor enhances the organization update functionality by allowing users to provide additional contact information and social media links.

--- src/organization/dto/update-organization.dto.ts
+++ src/organization/dto/update-organization.dto.ts
@@ -1,4 +1,41 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrganizationDto } from './create-organization.dto';
+import { IsEmail, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateOrganizationDto extends PartialType(CreateOrganizationDto) {}
+export class IOrganizationSocialMedia {
+    @IsString()
+    @IsOptional()
+    facebookUrl?: string;
+
+    @IsString()
+    @IsOptional()
+    linkedInUrl?: string;
+
+    @IsString()
+    @IsOptional()
+    xUrl?: string;
+
+    @IsString()
+    @IsOptional()
+    instagramUrl?: string;
+}
+
+export class UpdateOrganizationDto extends PartialType(CreateOrganizationDto) {
+
+    @IsEmail()
+    contactEmail: string;
+
+    @IsString()
+    @IsOptional()
+    contactPhone?: string;
+
+    @IsString()
+    @IsOptional()
+    myWebsiteUrl?: string
+    
+    @IsOptional()
+    @ValidateNested()
+    @Type(() => IOrganizationSocialMedia)
+    socialMedias?: IOrganizationSocialMedia;
+}
--- src/organization/entities/organization.entity.ts
+++ src/organization/entities/organization.entity.ts
@@ -2,31 +2,51 @@ import { Collaborator } from "src/collaborator/entities/collaborator.entity";
 import { Event } from "src/event/entities/event.entity";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
-
-@Entity('organizations')
+ type IOrganizationSocialMedia = {
+  facebookUrl?: string;
+  linkedInUrl?: string;
+  xUrl?: string;
+  instagramUrl?: string;
+};
+@Entity("organizations")
 export class Organization {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @Column({ unique: true })
+  name: string;
 
-    @Column({unique: true})
-    name: string;
+  @Column({ nullable: true, default: "" })
+  avatar: string;
 
-    @Column()
-    avatar: string;
-    
-    @OneToMany(() => Collaborator, collaborator => collaborator.organization)
-    collaborators: Collaborator[];
+  @OneToMany(() => Collaborator, (collaborator) => collaborator.organization)
+  collaborators: Collaborator[];
 
-    @OneToMany(() => Event, event => event.organization)
-    events: Event[];
+  @OneToMany(() => Event, (event) => event.organization)
+  events: Event[];
 
-    @Column({default: new Date()})
-    created_at: Date;
+  @Column({ nullable: true, default: "" })
+  contactEmail: string;
 
-    @Column({default: new Date()})
-    updated_at: Date;
+  @Column({ nullable: true, default: "" })
+  contactPhone?: string;
 
-    @Column({nullable: true})
-    deleted_at: Date;
+  @Column({ nullable: true, default: "" })
+  myWebsiteUrl?: string;
+
+  @Column({
+    nullable: true,
+    type: "simple-json",
+    default: { facebookUrl: "", linkedInUrl: "", xUrl: "", instagramUrl: "" },
+  })
+  socialMedias?: IOrganizationSocialMedia;
+
+  @Column({ default: new Date() })
+  created_at: Date;
+
+  @Column({ default: new Date() })
+  updated_at: Date;
+
+  @Column({ nullable: true })
+  deleted_at: Date;
 }
