51ec7a2b996a757095b11194944bb16ba8ff6f37
Luis Ortiz <ortizfrancoluisjavier@gmail.com>
2024-11-15 16:43:21 -0500

    se agrega endpoint de traer un asistente por id

--- src/attendee/attendee.controller.ts
+++ src/attendee/attendee.controller.ts
@@ -7,45 +7,45 @@ import { checkInDto } from './dto/check-in.dto';
 
 @Controller('attendee')
 export class AttendeeController {
-  constructor(private readonly attendeeService: AttendeeService) {}
+	constructor(private readonly attendeeService: AttendeeService) {}
 
-  
+	@Get()
+	findAll() {
+		return this.attendeeService.findAll();
+	}
 
-  @Get()
-  findAll() {
-    return this.attendeeService.findAll();
-  }
-  
-  @WithoutAccount()
-  @Get('find/:email')
-  findOne(@Param('email') email: string) {
-    return this.attendeeService.findOneByEmail(email);
-  }
+	@WithoutAccount()
+	@Get('find/:attendeeId')
+	findOne(@Param('attendeeId') attendeeId: string) {
+		return this.attendeeService.findOneById(attendeeId);
+	}
 
-  @Role(Roles.auditor)
-  @Get(':orgId/all/:eventId')
-  getAssistant(
-    @Query() pagination: PaginationArgs,
-    @Param('eventId') eventId: string) {
-    return this.attendeeService.getAttendeeByEvent(eventId,pagination);
-  }
+	@WithoutAccount()
+	@Get('find/eventId/:eventId/identify/:email')
+	findByEmail(@Param('email') email: string, @Param('eventId') eventId: string) {
+		return this.attendeeService.findOneByEmail(eventId, email);
+	}
 
-  @WithoutAccount()
-  @Get('totalCount/:eventId')
-  count(@Param('eventId') eventId: string) {
-    return this.attendeeService.getTotalAttendeesByEvent(eventId);
-  }
-  
-  @WithoutAccount()
-  @Patch('checkIn/:id')
-  update(
-    @Param('id') id: string,
-    @Body() CheckInDto: checkInDto) {
-    return this.attendeeService.checkIn(id,CheckInDto);
-  }
-  
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.attendeeService.remove(+id);
-  }
+	@Role(Roles.auditor)
+	@Get(':orgId/all/:eventId')
+	getAssistant(@Query() pagination: PaginationArgs, @Param('eventId') eventId: string) {
+		return this.attendeeService.getAttendeeByEvent(eventId, pagination);
+	}
+
+	@WithoutAccount()
+	@Get('totalCount/:eventId')
+	count(@Param('eventId') eventId: string) {
+		return this.attendeeService.getTotalAttendeesByEvent(eventId);
+	}
+
+	@WithoutAccount()
+	@Patch('checkIn/:id')
+	update(@Param('id') id: string, @Body() CheckInDto: checkInDto) {
+		return this.attendeeService.checkIn(id, CheckInDto);
+	}
+
+	@Delete(':id')
+	remove(@Param('id') id: string) {
+		return this.attendeeService.remove(+id);
+	}
 }
--- src/attendee/attendee.service.ts
+++ src/attendee/attendee.service.ts
@@ -1,102 +1,113 @@
-import { Injectable, InternalServerErrorException } from "@nestjs/common";
-import { AssistantDto } from "./dto/create-assistant.dto";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Attendee } from "./entities/attendee.entity";
-import { Repository } from "typeorm";
-import { PaginationArgs } from "src/common/dto";
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { AssistantDto } from './dto/create-assistant.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Attendee } from './entities/attendee.entity';
+import { Repository } from 'typeorm';
+import { PaginationArgs } from 'src/common/dto';
 import { CheckIn } from './entities/checkIn.entity';
-import { checkInDto } from "./dto/check-in.dto";
+import { checkInDto } from './dto/check-in.dto';
 
 @Injectable()
 export class AttendeeService {
-  constructor(
-    @InjectRepository(Attendee)
-    private attendeeRepository: Repository<Attendee>,
-    @InjectRepository(CheckIn)
-    private CheckInRepository: Repository<CheckIn>,
-  ) {}
+	constructor(
+		@InjectRepository(Attendee)
+		private attendeeRepository: Repository<Attendee>,
+		@InjectRepository(CheckIn)
+		private CheckInRepository: Repository<CheckIn>
+	) {}
 
-  async create(createAssistantDto: AssistantDto) {
-    const attendee = this.attendeeRepository.create(createAssistantDto);
-    return await this.attendeeRepository.save(attendee);
-  }
+	async create(createAssistantDto: AssistantDto) {
+		const attendee = this.attendeeRepository.create(createAssistantDto);
+		return await this.attendeeRepository.save(attendee);
+	}
 
-  findAll() {
-    return `This action returns all assistant`;
-  }
+	findAll() {
+		return `This action returns all assistant`;
+	}
 
-  async findOneByEmail(email: string) {
-    const attendee = await this.attendeeRepository.findOneBy({
-      user: {
-        email,
-      },
-    });
-    return attendee;
-  }
-  async checkIn(id: string, checkinData: checkInDto) {
-    try {
-      const {type,experienceID,stationID} = checkinData
-      const Attendee = await this.attendeeRepository.findOneBy({id});
-      const newCheckIn = this.CheckInRepository.create({
-        Attendee,
-        type,
-        experienceID,
-        stationID
-      })
-      await this.CheckInRepository.save(newCheckIn);
-      return { message: "check in successfully" };
-    } catch (error) {
-      throw new InternalServerErrorException("error updating assistant");
-    }
-  }
+	async findOneById(attendeeId: string) {
+		const attendee = await this.attendeeRepository.findOneBy({
+			id: attendeeId,
+		});
+		return { attendee };
+	}
 
-  async getAttendeeByEvent(eventId: string, pagination: PaginationArgs) {
-    const { offset, limit } = pagination;
+	async findOneByEmail(eventId: string, email: string) {
+		const attendee = await this.attendeeRepository.findOneBy({
+			user: {
+				email,
+			},
+			event: {
+				id: eventId,
+			},
+		});
 
-    const [attendees, total] = await this.attendeeRepository.findAndCount({
-      where: {
-        event: {
-          id: eventId,
-        },
-      },
-      take: limit,
-      skip: (offset - 1) * limit,
-    });
+		return { attendee };
+	}
+	async checkIn(id: string, checkinData: checkInDto) {
+		try {
+			const { type, experienceID, stationID } = checkinData;
+			const Attendee = await this.attendeeRepository.findOneBy({ id });
+			const newCheckIn = this.CheckInRepository.create({
+				Attendee,
+				type,
+				experienceID,
+				stationID,
+			});
+			await this.CheckInRepository.save(newCheckIn);
+			return { message: 'check in successfully' };
+		} catch (error) {
+			throw new InternalServerErrorException('error updating assistant');
+		}
+	}
 
-    return {
-      attendees: attendees.map((attendee) => ({
-        id: attendee.id,
-        fullName: attendee.fullName,
-        checkIn: attendee.checkIn,
-        email: attendee.user.email,
-        checkInAt: attendee.checkIn,
-      })),
-      total,
-    };
-  }
-  async getTotalAttendeesByEvent(eventId: string) {
-    const totalAttendee = await this.attendeeRepository.count({
-      where: {
-        event: {
-          id: eventId,
-        },
-      },
-    });
-    return { totalAttendee };
-  }
+	async getAttendeeByEvent(eventId: string, pagination: PaginationArgs) {
+		const { offset, limit } = pagination;
 
-  async findOneByUserIdAndEventId(userID: string, event: string) {
-    return await this.attendeeRepository.findOneBy({
-      user: {
-        id: userID,
-      },
-      event: {
-        id: event,
-      },
-    });
-  }
+		const [attendees, total] = await this.attendeeRepository.findAndCount({
+			where: {
+				event: {
+					id: eventId,
+				},
+			},
+			take: limit,
+			skip: (offset - 1) * limit,
+		});
 
-  remove(id: number) {
-    return `This action removes a #${id} assistant`;
-  }
+		return {
+			attendees: attendees.map((attendee) => ({
+				id: attendee.id,
+				fullName: attendee.fullName,
+				checkIn: attendee.checkIn,
+				email: attendee.user.email,
+				checkInAt: attendee.checkIn,
+			})),
+			total,
+		};
+	}
+	async getTotalAttendeesByEvent(eventId: string) {
+		const totalAttendee = await this.attendeeRepository.count({
+			where: {
+				event: {
+					id: eventId,
+				},
+			},
+		});
+		return { totalAttendee };
+	}
+
+	async findOneByUserIdAndEventId(userID: string, event: string) {
+		return await this.attendeeRepository.findOneBy({
+			user: {
+				id: userID,
+			},
+			event: {
+				id: event,
+			},
+		});
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} assistant`;
+	}
 }
