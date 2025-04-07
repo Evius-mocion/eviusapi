c100b5946376517214f48e75287d9c624765af73
Fredy_Pastrana <fdpastrana@hotmail.com>
2025-04-07 09:48:22 -0500

    feat(bingo): add TypeORM entities to Bingo module

--- src/bingo/bingo.module.ts
+++ src/bingo/bingo.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BingoService } from './bingo.service';
 import { BingoController } from './bingo.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Bingo } from './entities/bingo.entity';
+import { BingoCard } from './entities/bingo_card.entity';
+import { Ballots } from './entities/ballots.entity';
+import { BingoRound } from './entities/bingo_round.entity';
+import { Figure } from './entities/figure.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Bingo,BingoCard,Ballots,BingoRound,Figure])],
   controllers: [BingoController],
   providers: [BingoService],
 })
