8e919b59cc7691300c64ca7231cdd0b1be3f17d2
lortiz24 <ortizfrancoluisjavier@gmail.com>
2025-04-08 11:06:10 -0500

    feat(hidden-points): add hidden point management to element hunt game
    Introduce functionality to add and record hidden points in the element hunt game. This includes validation, collision detection, and UUID generation for points. The changes enable tracking of found points during game sessions.

--- src/element-hunt-game/dto/update-element-hunt-game.dto.ts
+++ src/element-hunt-game/dto/update-element-hunt-game.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateElementHuntGameDto } from './create-element-hunt-game.dto';
+import { Exclude } from 'class-transformer';
+import { IsOptional } from 'class-validator';
 
-export class UpdateElementHuntGameDto extends PartialType(CreateElementHuntGameDto) {}
+export class UpdateElementHuntGameDto extends PartialType(CreateElementHuntGameDto) {
+	@ApiProperty({ required: false })
+	@IsOptional()
+	@Exclude()
+	hidden_points?: never;
+}
