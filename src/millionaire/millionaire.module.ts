433481a5e51ce69a7925398b25c3f69abc4c8818
gitrubio <carloviloria0@gmail.com>
2025-04-15 15:13:54 -0500

    feat(millionaire): add TypeORM entities and module imports

--- src/millionaire/millionaire.module.ts
+++ src/millionaire/millionaire.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MillionaireService } from './millionaire.service';
 import { MillionaireController } from './millionaire.controller';
+import { Millionaire } from './entities/millionaire.entity';
+import { MillionaireQuestion } from './entities/millionaire_question.entity';
+import { MillionaireOption } from './entities/millionaire_options.entity';
+import { MillionaireAnswer } from './entities/millionaire_answer.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Millionaire, MillionaireQuestion, MillionaireOption, MillionaireAnswer])],
   controllers: [MillionaireController],
   providers: [MillionaireService],
 })
