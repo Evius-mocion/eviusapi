9f28a39ebb62ac0f886a7bc7a1fb0f139e5c758a
gitrubio <carloviloria0@gmail.com>
2025-04-28 15:51:35 -0500

    delete decorator RelationId

--- src/networking/entities/meeting-participant.entity.ts
+++ src/networking/entities/meeting-participant.entity.ts
@@ -3,23 +3,21 @@ import { Meeting } from './meeting.entity';
 import { Attendee } from 'src/attendee/entities/attendee.entity';
 
 @Entity('meeting_participant')
-export class MeetingParticipant {
-	@PrimaryGeneratedColumn('uuid')
-	id: string;
+	export class MeetingParticipant {
+		@PrimaryGeneratedColumn('uuid')
+		id: string;
 
 	@ManyToOne(() => Meeting, (meeting) => meeting.participants)
 	@JoinColumn({ name: 'meeting_id' })
 	meeting: Meeting;
 
-	@RelationId((meetingParticipant: MeetingParticipant) => meetingParticipant.meeting)
-	meetingId: string;
+	
 
 	@ManyToOne(() => Attendee, { eager: false, onDelete: 'CASCADE' })
 	@JoinColumn({ name: 'attendee_id' })
 	attendee: Attendee;
+							
 
-	@RelationId((mp: MeetingParticipant) => mp.attendee)
-	attendeeId: string;
 
 	@CreateDateColumn({ type: 'timestamp' })
 	created_at: Date;
